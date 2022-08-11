f042b2abfab7a0dd46f4215f34dafab150bd287d
Gustavo Teichmann <gustavo.teichmann@dbccompany.com.br>
2022-08-11 11:07:53 -0300

    ajuste no projeto cypress bdd

--- cypress-cucumber/cypress/support/steps/CreateStep.js
+++ cypress-cucumber/cypress/support/steps/CreateStep.js
@@ -94,90 +94,9 @@ When("clico no botao register", () => {
 
 }); 
 
-//////////////////////////////////////////////////////
-//////////////////////////////////////////////////////
-//////////////////////////////////////////////////////
-
-Given("que acesso o respectivo sistema {}", (url) => {
-  authPage.navegar(url);
-  authPage.preencherEmail();
-  authPage.clicarNoCreateBtn();
-  utils.tempo(10000);
-});
-
-And("clico no Mr.", () => {
-  createPage.clicarNoMrBtn();
-});
-
-And("preencho o campo first Name", () => {
-  createPage.preencherFirstName();
-});
-
-And("preencho o campo last Name", () => {
-  createPage.preencherLastName();
-});
-
-And("seleciono o respectivo dia", () => {
-  createPage.clicarNoDia();
-});
-
-And("seleciono o respectivo mes", () => {
-  createPage.clicarNoMes();
-
-});
-
-And("seleciono o respectivo ano", () => {
-  createPage.clicarNoAno();
-});
-
-And("preencho o campo first Name Address", () => {
-  createPage.preencherFirstNameAddress();
-});
-
-And("preencho o campo last Name Address", () => {
-  createPage.preencherLastNameAddress();
-});
-
-And("preencho o campo company", () => {
-  createPage.preencherCompany();
-});
-
-And("preencho o campo Address", () => {
-  createPage.prencherAddress();
-});
-
-And("preencho o campo City", () => {
-  createPage.preencherCity();
-});
-
-And("clico no respectivo country", () => {
-  createPage.clicarNoCountry();
-
-});
-
-And("preencho o campo postal code", () => {
-  createPage.preencherPostalCode();
-});
-
-
-And("clico no respectivo estado", () => {
-  createPage.clicarNoState();
-});
-
-And("preencho o campo mobile phone", () => {
-  createPage.preencherMobilePhone();
-
-});
-
-And("preencho o campo reference", () => {
-  createPage.preencherReference();
-
-});
-
-When("clico no botao register", () => {
-  createPage.clicarNoRegisterBtn();
-});
-
 Then("devo verificar mensagem de quando o campo password esta vazio", () => {
   createPage.verificarPasswordMsg();
 });
+
+
+
